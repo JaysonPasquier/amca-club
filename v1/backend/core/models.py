704ff33d80c0fb8fab25e426d1b6c445de36d713
Your Name <your.email@example.com>
2025-05-01 19:00:41 +0200

    change admin view

--- v1/backend/core/models.py
+++ v1/backend/core/models.py
@@ -190,17 +190,6 @@ class ProductImage(models.Model):
     class Meta:
         verbose_name = "Image de produit"
         verbose_name_plural = "Images de produit"
-    product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name='colors')
-    name = models.CharField(max_length=50)  # e.g., "Red", "Blue"
-    color_code = models.CharField(max_length=10, blank=True)  # Hex color code
-    image = models.ImageField(upload_to='products/colors/', blank=True, null=True)
-
-    def __str__(self):
-        return f"{self.name}"
-
-    class Meta:
-        ordering = ['name']
-        unique_together = ['product', 'name']
 
 class ProductSize(models.Model):
     SIZE_CHOICES = [
